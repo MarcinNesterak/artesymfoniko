c8d8607fad86a5ff9e3eb7c97b1d4eefd4a70ec3
marcin <marcin@MacBook-Pro-marcin.local>
2025-06-02 10:53:13 +0200

    Add content to Message.js model - fix empty file causing server crash

---
+++ orchestra-backend/models/Message.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const messageSchema = new mongoose.Schema({
+  eventId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Event',
+    required: true
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  content: {
+    type: String,
+    required: true,
+    maxlength: 500
+  }
+}, {
+  timestamps: true
+});
+
+export default mongoose.model('Message', messageSchema);
