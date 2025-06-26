d823b11e47b847eea91d2b85ecbfe09baf4d52de
marcin <marcin@MacBook-Pro-marcin.local>
2025-06-26 22:50:54 +0200

    fix(backend): Correct import method for auth middleware

--- orchestra-backend/routes/privateMessages.js
+++ orchestra-backend/routes/privateMessages.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import auth from '../middleware/auth.js';
+import { requireUser as auth } from '../middleware/auth.js';
 import User from '../models/User.js';
 import PrivateMessage from '../models/PrivateMessage.js';
 import mongoose from 'mongoose';
